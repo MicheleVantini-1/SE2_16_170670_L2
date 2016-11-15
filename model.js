8731cdccd0f6885b2b2372e0b61d7768a45beadd
MicheleVantini-1 <michele.vantini-1@studenti.unitn.it>
2016-11-15 20:48:34 +0100

    added the data structure to keep warehouse limit; setItemsLimit function added

--- model.js
+++ model.js
@@ -1,1 +1,42 @@
-warehouse = {}
+// MODEL
+
+// The following object will contain all 
+// the product in our warehouse
+warehouse = {};
+
+// this variable will maintain the total
+// number of items in the warehouse
+counter = 0;
+
+// This is the definition of the maximum default
+// number of items that our warehouse can contain
+defaultLimit = 50;
+limit = 0;
+setItemsLimit(defaultLimit);
+
+/**
+* Function that set the maximum number of product in the warehouse
+* @param l - the maximum value of item allowed in the warehouse
+*/
+function setItemsLimit(l)
+{
+	// setting the new limit
+	limit = parseInt(l);
+	document.getElementById("limitLabel").innerHTML = limit;
+
+	// since the limit has changed, the number of
+	// items in the warehouse can now exceed the limit
+	// If it is the case we alert the user
+	if(counter == limit)
+	{
+		alert("The warehouse is now full");
+	}
+	else if(counter > limit)
+	{
+		alert("The warehouse limit has been exceeded");				
+	}
+
+	// setting the two input html tag value to empty		
+	document.getElementById('productQuantityLimit').value = "";
+
+}
