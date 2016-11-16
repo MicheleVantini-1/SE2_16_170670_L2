e99c991e7d982f535ae4be919b7696b30b7eabc7
michele <michele.vantini-1@studenti.unitn.it>
2016-11-16 13:49:48 +0100

    checks on input data for the function addProduct added in script.js

--- script.js
+++ script.js
@@ -5,44 +5,55 @@
 * @param quantity - the number of units of the new product to add
 */
 function addProduct(name, quantity)
-{	
-	// we have to check if the product
-	// is not already present in the warehouse
-	if(warehouse[name] == undefined)
+{
+	// at first check if the name parameter is a non empty string
+	// and if the quantity is a positive integer(if it is a double 
+	// it will be casted to int)
+	if(name.length == 0 || quantity.length == 0 || isNaN(parseInt(quantity)) || parseInt(quantity) <= 0)
 	{
-		// if the product is new we add it to the warehouse
-		// and then we add it to the html table
-		warehouse[name] = quantity;
-
-		// Getting the table where we have to add the product
-		var table = document.getElementById("warehouse");
-		var rowsLength = table.rows.length;
-
-		// Adding a new row in the table
-		var row = table.insertRow(rowsLength);
-
-		// Adding two cells to the new row
-		var nameCell = row.insertCell(0);
-		var quantityCell = row.insertCell(1);
-		quantityCell.id = "product_" + name;
-
-		// Setting the value of the new row cell
-		nameCell.innerHTML = name;
+		// If the input values are invalid we alert the user
+		alert("The name of the product must be a NON EMPTY string" 
+			+ "\nand the quantity must be a POSITIVE integer ");
 	}
 	else
 	{
-		// if the product is already present
-		// we simply update its quantity
-		warehouse[name] = parseInt(warehouse[name]) + parseInt(quantity);
-	}
+		// we have to check if the product
+		// is not already present in the warehouse
+		if(warehouse[name] == undefined)
+		{
+			// if the product is new we add it to the warehouse
+			// and then we add it to the html table
+			warehouse[name] = quantity;
 
-	var quantityCell = document.getElementById("product_" + name);
-	quantityCell.innerHTML = warehouse[name];
-		
+			// Getting the table where we have to add the product
+			var table = document.getElementById("warehouse");
+			var rowsLength = table.rows.length;
 
-	// setting the two input html tags values to empty
-	document.getElementById('productName').value = "";
-	document.getElementById('productQuantity').value = "";
+			// Adding a new row in the table
+			var row = table.insertRow(rowsLength);
+
+			// Adding two cells to the new row
+			var nameCell = row.insertCell(0);
+			var quantityCell = row.insertCell(1);
+			quantityCell.id = "product_" + name;
+
+			// Setting the value of the new row cell
+			nameCell.innerHTML = name;
+		}
+		else
+		{
+			// if the product is already present
+			// we simply update its quantity
+			warehouse[name] = parseInt(warehouse[name]) + parseInt(quantity);
+		}
+
+		var quantityCell = document.getElementById("product_" + name);
+		quantityCell.innerHTML = warehouse[name];		
+
+		// setting the two input html tags values to empty
+		document.getElementById('productName').value = "";
+		document.getElementById('productQuantity').value = "";
+	}	
 }
 
 
